2ff7a5e00c7ac8e2dbdf0b5a0634de64bbd3280d
Snoussi Youssef <snyoussef07@gmail.com>
2020-11-23 11:20:07 +0100

    Commit initial

---
+++ src/main/java/be/bt/cinemasnoussapi/CinemaSnoussApiApplication.java
@@ -0,0 +1,13 @@
+package be.bt.cinemasnoussapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSnoussApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSnoussApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/bt/cinemasnoussapi/CinemaSnoussApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bt.cinemasnoussapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSnoussApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
