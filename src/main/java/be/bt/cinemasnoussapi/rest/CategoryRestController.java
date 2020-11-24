48adbc85c7fd0a553c450d4e118d648e5796d3f8
Snoussi Youssef <snyoussef07@gmail.com>
2020-11-24 12:41:47 +0100

    création des CRUD endpoints pour la class Category

--- src/main/java/be/bt/cinemasnoussapi/rest/CategoryRestController.java
+++ src/main/java/be/bt/cinemasnoussapi/rest/CategoryRestController.java
@@ -1,9 +1,13 @@
 package be.bt.cinemasnoussapi.rest;
 
+import be.bt.cinemasnoussapi.domain.Category;
 import be.bt.cinemasnoussapi.repository.ICategoryrepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
@@ -14,5 +18,53 @@ public class CategoryRestController {
 
     public CategoryRestController(ICategoryrepository categoryrepository) {
         this.categoryrepository = categoryrepository;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategorys() {
+        return categoryrepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable("id") Long id) {
+        Optional<Category> result = categoryrepository.findById(id);
+        if (result.isPresent()) {
+            return new ResponseEntity<Category>(result.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@RequestBody Category room) {
+        Optional<Category> result = categoryrepository.findById(room.getId());
+        if (!result.isPresent()) {
+            categoryrepository.save(room);
+            return new ResponseEntity<Category>(room, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Category> update(@RequestBody Category room) {
+        Optional<Category> result = categoryrepository.findById(room.getId());
+        if (result.isPresent()) {
+            categoryrepository.save(room);
+            return new ResponseEntity<Category>(room, HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Category> deleteById(@PathVariable("id") Long id) {
+        Optional<Category> result = categoryrepository.findById(id);
+        if (result.isPresent()) {
+            categoryrepository.delete(result.get());
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
