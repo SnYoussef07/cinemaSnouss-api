128fe21fb2614224a4b61b11bc2a42aee8a817e3
Snoussi Youssef <snyoussef07@gmail.com>
2020-11-24 12:42:47 +0100

    création des CRUD endpoints pour la class Movie

--- src/main/java/be/bt/cinemasnoussapi/rest/MovieRestController.java
+++ src/main/java/be/bt/cinemasnoussapi/rest/MovieRestController.java
@@ -1,9 +1,13 @@
 package be.bt.cinemasnoussapi.rest;
 
+import be.bt.cinemasnoussapi.domain.Movie;
 import be.bt.cinemasnoussapi.repository.IMovieRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -14,5 +18,53 @@ public class MovieRestController {
 
     public MovieRestController(IMovieRepository movieRepository) {
         this.movieRepository = movieRepository;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Movie> findById(@PathVariable("id") Long id) {
+        Optional<Movie> result = movieRepository.findById(id);
+        if (result.isPresent()) {
+            return new ResponseEntity<Movie>(result.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie room) {
+        Optional<Movie> result = movieRepository.findById(room.getId());
+        if (!result.isPresent()) {
+            movieRepository.save(room);
+            return new ResponseEntity<Movie>(room, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Movie> update(@RequestBody Movie room) {
+        Optional<Movie> result = movieRepository.findById(room.getId());
+        if (result.isPresent()) {
+            movieRepository.save(room);
+            return new ResponseEntity<Movie>(room, HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Movie> deleteById(@PathVariable("id") Long id) {
+        Optional<Movie> result = movieRepository.findById(id);
+        if (result.isPresent()) {
+            movieRepository.delete(result.get());
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
